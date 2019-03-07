52fdc8c24daf8a1002201c4ec910ebb343b2cb78
flamegiraffe <flamegiraffes@gmail.com>
2019-03-07 15:55:35 -0800

    finishe product

---
+++ Assets/02_Scripts/GolfballScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfballScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision c){
+		Debug.Log("collided");
+	// 	foreach(ContactPoint cp in c.contacts){
+	// 		if(cp.otherCollider.transform.root.tag=="Player"){
+	// 			Debug.Log("collided with something player");
+	// 			Vector3 ocp = cp.otherCollider.transform.position;
+	// 			Vector3 d = transform.position-new Vector3(ocp.x, transform.position.y, ocp.z);
+	// 			Debug.Log(d);
+	// 			GetComponent<Rigidbody>().AddForce(50*(d.normalized), ForceMode.Acceleration);
+	// 		}
+	// 	}
+	}
+}
---
+++ Assets/02_Scripts/Respawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour {
+	public GameObject ballPrefab;
+	public Transform originalPosition;
+	public void OnTriggerExit(Collider c){
+		if(c.transform.root.tag=="Ball"){
+			GameObject b = Instantiate(ballPrefab) as GameObject;
+			b.transform.position = originalPosition.position;
+		}
+	}
+}

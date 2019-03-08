ef2f2f7767a72a7ebc261e646f5ce6006e097553
flamegiraffe <flamegiraffes@gmail.com>
2019-03-07 17:50:27 -0800

    removed some for workshop

--- Assets/02_Scripts/Respawner.cs
+++ Assets/02_Scripts/Respawner.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Respawner : MonoBehaviour {
 	public GameObject ballPrefab;
 	public Transform originalPosition;
+
 	public void OnTriggerExit(Collider c){
 		if(c.transform.root.tag=="Ball"){
-			GameObject b = Instantiate(ballPrefab) as GameObject;
-			b.transform.position = originalPosition.position;
+			//TODO: WRITE INFINIBALLS CODE HERE
 		}
 	}
 }
--- Assets/02_Scripts/WindmillScript.cs
+++ Assets/02_Scripts/WindmillScript.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class WindmillScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-
 	// Update is called once per frame
 	void Update () {
-		transform.RotateAround(transform.position, transform.right, Time.deltaTime*40f);
+		//TODO: WRITE WINDMILL SPINNY LINE HERE
 	}
 }
